9e68e133b2b22f6f91dc2c64016958c35005bf3e
Carlos <inf.carlos@outlook.com>
2022-12-07 01:48:41 +0300

    add animations

--- player/MainPlayer.gd
+++ player/MainPlayer.gd
@@ -6,21 +6,28 @@ export var FRICTION = 300
 export var ROTATION_SPEED = 1.5
 
 enum {
-	MOVE
+	MOVE,
+	COLLECT_LEFT,
+	COLLECT_RIGHT
 }
 
 var state = MOVE
 var velocity = Vector2.ZERO
 var rotation_dir = 0
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
  
 func _ready():
   velocity = Vector2.ZERO
-  
  
 func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
+		COLLECT_LEFT:
+			collect_state_left(delta)
+		COLLECT_RIGHT:
+			collect_state_right(delta)
 
 func move_state(delta):
 	rotation_dir = 0
@@ -29,23 +36,36 @@ func move_state(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	if(Input.is_action_pressed("ui_left")):
+	if Input.is_action_pressed("ui_left"):
 		rotation_dir -= 1
 
-	if(Input.is_action_pressed("ui_right")):
+	if Input.is_action_pressed("ui_right"):
 		rotation_dir += 1
 		
-	if(Input.is_action_pressed("ui_up")):
+	if Input.is_action_pressed("ui_up"):
 		velocity = velocity.move_toward(input_vector.rotated(rotation) * MAX_SPEED, ACCELERATION * delta)
 
-	if(Input.is_action_pressed("ui_down")):
+	if Input.is_action_pressed("ui_down"):
 		velocity = velocity.move_toward(input_vector.rotated(rotation) * MAX_SPEED, ACCELERATION * delta)
 		
 	if input_vector.y == 0:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move(delta)
+	
+	if Input.is_action_just_pressed("pick_left"):
+		state = COLLECT_LEFT
+		
+	if Input.is_action_just_pressed("pick_right"):
+		state = COLLECT_RIGHT
 
 func move(delta):
 	rotation += rotation_dir * ROTATION_SPEED * delta
 	velocity = move_and_slide(velocity)
+	
+func collect_state_left(delta):
+	animationState.travel("collectLeft")
+	
+func collect_state_right(delta):
+	animationState.travel("collectRight")
+
